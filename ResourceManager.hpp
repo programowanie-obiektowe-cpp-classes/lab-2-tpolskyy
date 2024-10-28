123e85447771009f5dcee9e47035c3ba87c703cc
Tomasz Polski <tomaszpolski21@gmail.com>
2024-10-28 13:48:02 +0100

    new file:   .vscode/tasks.json 	modified:   ResourceManager.hpp 	new file:   catch.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,42 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+    Resource* resource; 
+
+public:
+   
+    ResourceManager()
+        : resource(new Resource()) 
+    {
+    }
+
+   
+    ResourceManager(const ResourceManager& other)
+        : resource(new Resource(*other.resource)) 
+    {
+    }
+
+    
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other)
+        {
+            delete resource;
+            resource = new Resource(*other.resource); 
+        }
+        return *this;
+    }
+
+   
+    ~ResourceManager()
+    {
+        delete resource; 
+    }
+
+    
+    double get()
+    {
+        return resource->get();
+    }
 };
